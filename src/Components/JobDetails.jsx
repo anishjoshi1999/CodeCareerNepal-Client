3f723fef80c4be8c49b1da8fbc1e6ac63575e3ca
Ashish <45767581+vattevaii@users.noreply.github.com>
2024-01-06 14:05:19 +0545

    fix: job detail

--- src/Components/JobDetails.jsx
+++ src/Components/JobDetails.jsx
@@ -37,35 +37,17 @@ function JobDetails() {
             }}
           />
         </div>
-        <div className="col-lg-6 mx-auto text-center mb-3">
-          <h1 className="display-4">{companyName?.toUpperCase()}</h1>
-          <p className="lead text-muted mb-0">Latest Job Openings.</p>
-          <div style={{ margin: "20px 0" }}>
-            <input
-              type="text"
-              placeholder="Search by Job Title"
-              ref={inputRef}
-              style={{
-                width: "100%",
-                padding: "10px",
-                borderRadius: "5px",
-                border: "1px solid #ccc",
-                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-              }}
-            />
-          </div>
-        </div>
-        <div className="row">
-          {filteredList.map((element, index) => (
-            <JobCard
-              key={index}
-              companyName={companyName}
-              jobName={element.jobName}
-              jobUrl={element.jobUrl}
-              updatedAt={element.updatedAt}
-            />
-          ))}
-        </div>
+      </div>
+      <div className="row">
+        {filteredList.map((element, index) => (
+          <JobCard
+            key={index}
+            companyName={companyName}
+            jobName={element.jobName}
+            jobUrl={element.jobUrl}
+            updatedAt={element.updatedAt}
+          />
+        ))}
       </div>
     </div>
   );
