667ecdc754846ff0dbe40bb53368a2cbb61ecd90
Anish.Joshi <anish.joshi@cedargate.com>
2024-03-09 11:02:42 +0545

    Updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -102,31 +102,6 @@ function About() {
                 </ul>
               </div>
             </div>
-
-            <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow-sm py-5 px-4">
-                <img
-                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png"
-                  alt=""
-                  width="100"
-                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-                />
-                <h5 className="mb-0">Ashish Bhattarai </h5>
-                <span className="small text-titlecase text-muted">Developer</span>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
 
           
